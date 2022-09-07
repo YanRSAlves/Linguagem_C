3a42ae6fac7488ebf22279a1e8f59ea5db5ce455
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-07 17:35:39 -0300

    update01

---
+++ condicionais_simples/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int a, b, c;
+
+    b = 50;
+    c = 60;
+
+    printf("Digite um numero: \n");
+    scanf("%d",&a);
+
+    if(a > 10){
+        printf("O numero %d e maior que 10 assim como o %d e %d \n", a, c, b);
+    }
+
+
+
+
+
+    return 0;
+}
