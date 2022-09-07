a26e19f7b149391a45167769bc139f2eccd7ac37
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-07 19:15:36 -0300

    update

---
+++ escolhas/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char escolha;
+
+
+    printf ("Menu:\n");
+    printf ("A - Cadastro\n");
+    printf ("I - Imprimir\n");
+    printf ("S - Sair\n\n");
+    printf ("Digite sua opcao: ");
+    scanf ("%c", &escolha);
+
+    switch (escolha) {
+        case 'A' :
+            printf("Cadastrando...\n");
+            break;
+        case 'I' :
+            printf("Imprimindo...\n");
+            break;
+        case 'S' :
+            printf("Saindo...\n");
+            break;
+        default :
+            printf("Opcao invalida\n");
+            break;
+    }
+
+
+
+    return 0;
+}
