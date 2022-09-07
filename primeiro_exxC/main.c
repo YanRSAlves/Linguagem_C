4651f377ec61edeeeffcfe8ce68f69ec2bbdc5be
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-07 16:33:08 -0300

    primeiro update

---
+++ primeiro_exxC/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int av1, av2, avd, resultado;
+    printf("=================================\n");
+
+    av1 = 6;
+    av2 = 6;
+    avd = 6;
+    resultado = (av1+av2+avd)/3;
+    printf("\n A media deste aluno e %d \t", resultado);
+
+    return 0;
+}
---
+++ valores/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int A, B, soma;
+
+    printf("\n Digite o valor 1: \t");
+    scanf("%d", &A);
+
+    printf("\n Digite o valor 2: \t");
+    scanf("%d", &B);
+
+    soma = A+B;
+
+    printf("\n A soma dos valores digitados e: \t %d", soma);
+
+
+
+
+
+
+    return 0;
+}
