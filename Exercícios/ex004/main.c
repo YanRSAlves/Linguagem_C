a0e87d4e5fe34a9aa717d808fc18016fce601adc
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-08 16:47:15 -0300

    ex004

---
+++ Exercícios/ex004/main.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("\t\tDESCUBRA O DIA DA SEMANA DIGITANDO A DATA DO MES DE SETEMBRO!\n\n\n");
+
+            char escolha;
+
+                printf("Digite o numero do dia que voce deseja saber: ");
+                scanf("%c",&escolha);
+            switch(escolha){
+                case '1':
+                    printf("\nDia %c Quinta-feira!\n", escolha);
+                break;
+
+                case '2':
+                    printf("\nDia %c Sexta-feira!\n", escolha);
+                break;
+
+                case '3':
+                    printf("\nDia %c Sabado!\n", escolha);
+                break;
+
+                case '4':
+                    printf("\nDia %c Domingo!\n", escolha);
+                break;
+
+                case '5':
+                    printf("\nDia %c Segunda-feira!\n", escolha);
+                break;
+
+                case '6':
+                    printf("\nDia %c Terca-feira!\n", escolha);
+                break;
+
+                case '7':
+                    printf("\nDia %c Quarta-feira!\n", escolha);
+                break;
+
+                case '8':
+                    printf("\nDia %c Quinta-feira!\n", escolha);
+                break;
+
+                case '9':
+                    printf("\nDia %c Sexta-feira!\n", escolha);
+                break;
+
+                case '10':
+                    printf("\nDia %c Sabado!\n", escolha);
+                break;
+
+                case '11':
+                    printf("\nDia %c Domingo!\n", escolha);
+                break;
+
+                case '12':
+                    printf("\nDia %c Segunda-feira!\n", escolha);
+                break;
+
+                case '13':
+                    printf("\nDia %c Terca-feira!\n", escolha);
+                break;
+
+                case '14':
+                    printf("\nDia %c Quarta-feira!\n", escolha);
+                break;
+
+                case '15':
+                    printf("\nDia %c Quinta-feira!\n", escolha);
+                break;
+
+                case '16':
+                    printf("\nDia %c Sexta-feira!\n", escolha);
+                break;
+
+                case '17':
+                    printf("\nDia %c Sabado!\n", escolha);
+                break;
+
+                case '18':
+                    printf("\nDia %c Domingo!\n", escolha);
+                break;
+
+                case '19':
+                    printf("\nDia %c Segunda-feira!\n", escolha);
+                break;
+
+                case '20':
+                    printf("\nDia %c Terca-feira!\n", escolha);
+                break;
+
+                case '21':
+                    printf("\nDia %c Quarta-feira!\n", escolha);
+                break;
+
+                case '22':
+                    printf("\nDia %c Quinta-feira!\n", escolha);
+                break;
+
+                case '23':
+                    printf("\nDia %c Sexta-feira!\n", escolha);
+                break;
+
+                case '24':
+                    printf("\nDia %c Sabado!\n", escolha);
+                break;
+
+                case '25':
+                    printf("\nDia %c Domingo!\n", escolha);
+                break;
+
+                case '26':
+                    printf("\nDia %c Segunda-feira!\n", escolha);
+                break;
+
+                case '27':
+                    printf("\nDia %c Terca-feira!\n", escolha);
+                break;
+
+                case '28':
+                    printf("\nDia %c Quarta-feira!\n", escolha);
+                break;
+
+                case '29':
+                    printf("\nDia %c Quinta-feira!\n", escolha);
+                break;
+
+                case '30':
+                    printf("\nDia %c Sexta-feira!\n", escolha);
+                break;
+
+                default :
+                    printf("NAO EXISTE ESSA DATA NO MES DE SETEMBRO!\n");
+                break;
+                }
+
+
+
+
+    return 0;
+}
