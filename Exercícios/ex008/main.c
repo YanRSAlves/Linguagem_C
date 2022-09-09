031dc8e7c9562decf686086fda97b1886eebb5a5
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-08 23:28:48 -0300

    m

---
+++ Exercícios/ex008/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+        int k;
+        k =  10;
+
+        for(k = 1;k <= 10; k = k+1) {
+            printf("%d %d %d \n",k,sqrt(k);
+        }
+
+
+    return 0;
+}
