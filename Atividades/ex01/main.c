44d03114ab6e1304b84bf04c708149453f76487b
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-15 20:14:56 -0300

    Update main.c

--- Atividades/ex01/main.c
+++ Atividades/ex01/main.c
@@ -3,6 +3,31 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    float a, b, h;
+    int p;
+
+    printf("====CALCULO DA AREA E PERIMETRO DE UM RETANGULO!!====\n\n\n");
+
+
+
+    printf("Digite a base: ");
+    scanf("%f", &b);
+
+    printf("Digite a altura: ");
+    scanf("%f", &h);
+
+    a = b*h;
+
+    printf("\n\nArea deste retangulo e %.1f!\n", a);
+
+    p = 2*b+2*h;
+
+    printf("e o perimetro sera %d!\n\n\n", p);
+
+
+
+
+
     return 0;
 }
