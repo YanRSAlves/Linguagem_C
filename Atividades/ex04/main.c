f5067271aa0998b0edb9050359efccea21e56084
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-15 23:07:08 -0300

    up

---
+++ Atividades/ex04/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float nome, cargo;
+    int idade;
+    float salario_liquido, salario_bruto;
+    float reajuste, gratificacao, descontado;
+
+   printf("==================ATUALIZACAO DE FORMULARIO=================");
+   printf("\ninforme as informacoes veridicas para nova analise!...\n\n\n");
+
+   printf("Qual eh o seu nome? ");
+   scanf("%f",&nome);
+
+   printf("%f",nome);
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
