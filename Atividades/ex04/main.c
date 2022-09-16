6e3d7a7f6854a41df8132ab97ff261c7c23ce38d
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-15 23:31:46 -0300

    0

--- Atividades/ex04/main.c
+++ Atividades/ex04/main.c
@@ -2,19 +2,29 @@
 #include <stdlib.h>
 
 int main()
-{
+{/*
     float nome, cargo;
     int idade;
     float salario_liquido, salario_bruto;
     float reajuste, gratificacao, descontado;
+    */
+    char nome[30];
 
    printf("==================ATUALIZACAO DE FORMULARIO=================");
    printf("\ninforme as informacoes veridicas para nova analise!...\n\n\n");
 
    printf("Qual eh o seu nome? ");
-   scanf("%f",&nome);
+   scanf("%s",&nome);
 
-   printf("%f",nome);
+   printf("Qual eh o seu nome? ");
+   scanf("%s",&nome);
+
+   printf("Qual eh o seu nome? ");
+   scanf("%s",&nome);
+
+   printf("Qual eh o seu nome? ");
+   scanf("%s",&nome);
+
 
 
 
