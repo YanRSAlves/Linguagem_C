4f6aecaeaa0b04e279774b50ba6a73820ee5ea92
Yan Rocha Silva Alves <110752168+YanRSAlves@users.noreply.github.com>
2022-09-18 11:31:52 -0300

    0

---
+++ fors/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int i;
+
+        for(i=1; i<=10; i++){
+            printf("%d ", i);
+        }
+
+
+
+
+
+
+
+
+    return 0;
+}
